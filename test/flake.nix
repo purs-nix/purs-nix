c12a053a406045e913a4b959df414d15c35e572d
Mason Mackaman <masondeanm@aol.com>
2021-04-08 16:04:49 -0400

    Update testing

--- test/flake.nix
+++ test/flake.nix
@@ -1,44 +1,35 @@
-{ inputs =
-    { purs-nix.url = "path:..";
+{ inputs.purs-nix.url = "path:..";
 
-      easy-ps =
-        { url = "github:justinwoo/easy-purescript-nix";
-          flake = false;
-        };
-    };
-
-  outputs = { nixpkgs, utils, easy-ps, purs-nix, ... }:
+  outputs = { nixpkgs, utils, purs-nix, ... }:
     utils.defaultSystems
       ({ pkgs, system }: with pkgs;
-         { defaultPackage =
-             let inherit (purs-nix { inherit system; }) purs ps-pkgs; in
-             (purs
-                { inherit pkgs system;
-                  deps =
-                    with ps-pkgs;
-                    [ console
-                      effect
-                      prelude
-                    ];
-
-                  src = ./src;
-                }
-             ).Main.bundle {};
-
-           devShell =
-             mkShell
-               { buildInputs =
-                   [ dhall
-                     nodejs
-                     purescript
-                   ]
-                   ++ (with import easy-ps { inherit pkgs; };
-                       [ spago
-                         spago2nix
-                       ]
-                      );
+           let
+             inherit (purs-nix { inherit system; }) purs ps-pkgs;
+             inherit
+               (purs
+                  { deps = with ps-pkgs; [ console effect prelude ];
+                    src = ./src;
+                  }
+               )
+               bundle
+               compile
+               modules;
+           in
+           { apps =
+               { bundle = bundle {};
+                 compile = compile {};
                };
-        }
+
+             defaultPackage = modules.Main.bundle {};
+
+             devShell =
+               mkShell
+                 { buildInputs =
+                     [ nodejs
+                       purescript
+                     ];
+                 };
+          }
       )
       nixpkgs;
 }
