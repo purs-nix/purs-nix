e3c846f98e38beb285a3bea648d66e9c76195f52
Mason Mackaman <masondeanm@aol.com>
2021-04-08 20:10:52 -0400

    Update packages

--- ps-pkgs-ns.nix
+++ ps-pkgs-ns.nix
@@ -2,6 +2,6 @@
   with ps-pkgs;
   { ursi =
       { debug = import ((builtins.fetchGit { rev = "c57b867dd6403dba3223e5db28c82c351757f4df"; url = "https://github.com/ursi/purescript-debuggest.git"; }) + /package.nix) pkgs;
-        prelude = import /home/mason/git/purescript-mason-prelude/package.nix args;
+        prelude = import ((builtins.fetchGit { rev = "281d8e7ab24ff840b08608d2d8ded457daf88b0d"; url = "https://github.com/ursi/purescript-mason-prelude.git"; }) + /package.nix) pkgs;
       };
   }
