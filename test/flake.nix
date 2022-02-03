182f14c79b719e71f3ed3a9a0fab25b2e21659c6
Mason Mackaman <masondeanm@aol.com>
2022-02-03 10:15:42 -0500

    Fix test flake
    Nix updated to no longer allow `path:..`

--- test/flake.nix
+++ test/flake.nix
@@ -1,9 +1,13 @@
 { inputs =
     { nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-      purs-nix.url = "path:..";
       utils.url = "github:ursi/flake-utils/2";
     };
 
   outputs = { utils, ... }@inputs:
-    utils.default-systems (import ./shared.nix) inputs;
+    utils.default-systems
+      ({ make-shell, pkgs, system, ... }:
+         let purs-nix = import ../. { inherit system; }; in
+         import ./shared.nix { inherit make-shell pkgs purs-nix; }
+      )
+      inputs;
 }
