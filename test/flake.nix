246fed01b67250adeb1eb0ae65850c630a79e1ce
Mason Mackaman <masondeanm@aol.com>
2022-05-18 19:02:13 -0400

    Use get-flake for the flake.nix test

--- test/flake.nix
+++ test/flake.nix
@@ -1,13 +1,15 @@
 { inputs =
-    { make-shell.url = "github:ursi/nix-make-shell/1";
+    { get-flake.url = "github:ursi/get-flake";
+      make-shell.url = "github:ursi/nix-make-shell/1";
       nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
       utils.url = "github:ursi/flake-utils/8";
     };
 
-  outputs = { utils, ... }@inputs:
-    utils.apply-systems { inherit inputs; }
-      ({ make-shell, pkgs, system, ... }:
-         let purs-nix = import ../. { inherit system; }; in
+  outputs = { get-flake, utils, ... }@inputs:
+    utils.apply-systems
+      { inputs = inputs // { purs-nix = get-flake ../.; };
+      }
+      ({ make-shell, pkgs, purs-nix, system, ... }:
          import ./shared.nix { inherit make-shell pkgs purs-nix; }
       );
 }
