d3f0f5054e5d4b9a30c3af714265c3a4a06fe18d
Mason Mackaman <masondeanm@aol.com>
2022-05-27 15:44:21 -0400

    Update to PureScript 0.14.9

--- purs-nix.nix
+++ purs-nix.nix
@@ -3,7 +3,7 @@ deps:
   let
     l = p.lib; p = pkgs; u = import ./utils.nix l;
     inherit (deps) builders easy-ps pkgs purescript-language-server;
-    purescript' = easy-ps.purescript;
+    purescript' = easy-ps.purs-0_14_9;
   in
   { inherit (import ./build-pkgs.nix { inherit pkgs; utils = u; })
       build ps-pkgs ps-pkgs-ns;
