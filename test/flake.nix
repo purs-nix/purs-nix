2510997fb6cd7b602faf89280a94089651ad0512
Mason Mackaman <masondeanm@aol.com>
2022-06-17 20:06:26 -0400

    Remove non-flake support from testing
    I would rather use a flake input than test non-flakes and have to work
    around not using one. I plan on removing official non-flake support at
    some point soonish anyway, since compatibility functions exists.
    
    I also did some slight tweaking to the flake and removed a file that is
    no longer used.

--- test/flake.nix
+++ test/flake.nix
@@ -13,10 +13,63 @@
 
   outputs = { get-flake, purs-nix-test-packages, utils, ... }@inputs:
     utils.apply-systems
-      { inputs = inputs // { purs-nix = get-flake ../.; };
-      }
+      { inputs = inputs // { purs-nix = get-flake ../.; }; }
       ({ make-shell, pkgs, purs-nix, ... }:
-         import ./shared.nix
-           { inherit make-shell pkgs purs-nix purs-nix-test-packages; }
+         let
+           inherit (purs-nix) build ps-pkgs;
+
+           ps =
+             purs-nix.purs
+               { dependencies =
+                   let
+                   in
+                   with ps-pkgs;
+                   [ console
+
+                     (build
+                        { name = "effect";
+                          src = purs-nix-test-packages;
+                          info = /effect/package.nix;
+                        }
+                     )
+
+                     (build
+                        { name = "prelude";
+
+                          src =
+                            { repo = "https://github.com/ursi/purs-nix-test-packages.git";
+                              rev = "25b3125cf4cac00feb6d8ba3b24c5f27271d42ff";
+                            };
+
+                          info = /prelude/package.nix;
+                        }
+                     )
+                   ];
+
+                 test-dependencies = [ ps-pkgs."assert" ];
+                 srcs = [ ./src ./src2 ];
+               };
+         in
+         { packages.default = ps.modules.Main.app { name = "test"; };
+
+           devShell =
+             make-shell
+               { packages =
+                   with pkgs;
+                   [ nodejs
+
+                     (ps.command
+                        { name = "purs-nix-test";
+                          package = import ./package.nix purs-nix;
+                          srcs = [ "src" "src2" ];
+                        }
+                     )
+
+                     purs-nix.esbuild
+                     purs-nix.purescript
+                     purs-nix.purescript-language-server
+                   ];
+               };
+         }
       );
 }
