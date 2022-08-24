13ac5cb8e02a03b3c8bb1b0a3555c9bdaa588912
Sridhar Ratnakumar <srid@srid.ca>
2022-08-24 14:58:04 -0400

    Add flake.nix

---
+++ docs/flake.nix
@@ -0,0 +1,36 @@
+{
+  nixConfig.extra-substituters = "https://cache.garnix.io";
+  nixConfig.extra-trusted-public-keys = "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=";
+
+  inputs = {
+    emanote.url = "github:EmaApps/emanote";
+    nixpkgs.follows = "emanote/nixpkgs";
+    flake-parts.follows = "emanote/flake-parts";
+  };
+
+  outputs = inputs@{ self, flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit self; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [
+        inputs.emanote.flakeModule
+      ];
+      perSystem = { self', pkgs, system, ... }: {
+        emanote = {
+          # By default, the 'emanote' flake input is used.
+          # package = inputs.emanote.packages.${system}.default;
+          sites."default" = {
+            path = ./.;
+            pathString = "./";
+            # port = 8080;
+            # baseUrl = "/mynotes";
+            # prettyUrls = true;
+          };
+        };
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nixpkgs-fmt
+          ];
+        };
+      };
+    };
+}
