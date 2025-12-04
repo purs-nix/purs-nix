7bdc233f02c8467250ec90c4bccfe8c45ae5d804
Mason Mackaman <masondeanm@aol.com>
2025-12-04 02:44:55 -0500

    Add aarch64-darwin as a supported system

--- flake.nix
+++ flake.nix
@@ -61,7 +61,7 @@
         };
       };
     }
-    // inputs.utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ]
+    // inputs.utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ]
       (system:
         let
           pkgs = inputs.nixpkgs.legacyPackages.${system};
